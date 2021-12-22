b6b80c8681f628b69f41f3b325736584a9d4d485
smun <seung.mun@gmail.com>
2021-12-22 14:44:18 -0500

    remote setup script

--- setup.sh
+++ setup.sh
@@ -7,14 +7,11 @@ DOTDIR=.dotfiles
 TDNOW=$(date +%s)
 
 # add files here:
-dotfiles=(
-    ".zshrc" \
-    ".p10k.zsh" \
-    ".config/nvim/init.vim"
-)
+dotfiles=(".zshrc" ".p10k.zsh" ".config/nvim/init.vim")
+homedirs=("bin" "lib" ".ssh" ".config" ".local")
 
 function help() {
-    echo "$0 help"
+    echo "$0 <install|update>"
 }
 
 function update() {
@@ -22,32 +19,45 @@ function update() {
     git pull
 }
 
+function populate_homedir() {
+    cd ${HOME}
+    for hdir in ${homedirs[@]}; do
+        [ ! -d ${HOME}/${hdir} ] && mkdir -p ${HOME}/${hdir}
+    done
+}
+    
 function install() {
     [ ! -d ${HOME}/${DOTDIR} ] && mkdir ${HOME}/${DOTDIR}
     git clone ${DOTGITREPO} ${HOME}/${DOTDIR}
     
     cd ${HOME}
+
+    populate_homedir
+
     for dfile in ${dotfiles[@]}; do
         if [ -f ${HOME}/${dfile} ]; then
             mv ${HOME}/${dfile} ${HOME}/${dfile}.save-${TDNOW}
         else
-            ln -sf ${DOTDIR}/${dfile} 
+	    parent_dir=$(dirname ${dfile})
+	    [ ${parent_dir} != "." ] && mkdir -p ${parent_dir}
+            ln -sf ${HOME}/${DOTDIR}/${dfile} ${dfile}
         fi
     done
 }
 
 progname=$0
-subcommand=$1
+subcomm=$1
 
-case ${subcommand} in
+case ${subcomm} in
     "" | "-h" | "--help")
         help
         ;;
     *)
         shift
-        ${subcommand} $@
+        ${subcomm} $@
         if [ $? = 127 ]; then
             echo "unknown command"
+	    help
             exit 1
         fi
 esac
